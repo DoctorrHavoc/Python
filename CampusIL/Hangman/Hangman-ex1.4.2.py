d313c55db758cdc377d0ab9fde6305f9da7c90d4
DoctorrHavoc <35706573+DoctorrHavoc@users.noreply.github.com>
2024-04-29 15:40:52 +0300

    Create Hangman-ex1.4.2.py

---
+++ CampusIL/Hangman/Hangman-ex1.4.2.py
@@ -0,0 +1,50 @@
+print(r"""picture 1:
+    x-------x
+""")
+print(r"""picture 2:
+    x-------x
+    |
+    |
+    |
+    |
+    |""")
+
+print(r"""picture 3:
+    x-------x
+    |       |
+    |       0
+    |
+    |
+    |""")
+
+print(r"""picture 4:
+    x-------x
+    |       |
+    |       0
+    |       |
+    |
+    |""")
+
+print(r"""picture 5:
+    x-------x
+    |       |
+    |       0
+    |      /|\
+    |
+    |""")
+
+print(r"""picture 6:
+    x-------x
+    |       |
+    |       0
+    |      /|\
+    |      /
+    |""")
+
+print(r"""picture 7:
+    x-------x
+    |       |
+    |       0
+    |      /|\
+    |      / \
+    |""")
